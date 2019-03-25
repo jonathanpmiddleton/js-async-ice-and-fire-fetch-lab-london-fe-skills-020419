dccb19edf80cb7653d49f4d2ca9596415852981b
Jm <jonathan.p.middleton@gmail.com>
2019-03-25 18:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   //write fetch request to the Game of Thrones API
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBookshtt(json));
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
