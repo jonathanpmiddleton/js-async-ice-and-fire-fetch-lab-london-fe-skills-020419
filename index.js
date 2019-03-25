17c61f5bb3dd0d6b0ec237c66b3a351dab6bdf8e
Jm <jonathan.p.middleton@gmail.com>
2019-03-25 18:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,20 +40,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function fetchCharacters() {
   //write fetch request to the Game of Thrones API
-  fetch('https://anapioficeandfire.com/api/houses')
+  fetch('https://anapioficeandfire.com/api/characters/1031')
   .then(resp => resp.json())
-  .then(json => renderHouses(json));
+  .then(json => renderCharacters(json));
 }
 
-function renderHouses(json) {
+function renderCharacters(json) {
   const main = document.querySelector('main');
-  json.forEach(house => {
+  json.forEach(characters => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = `<h2>${house.name}</h2>`;
+    h2.innerHTML = `<h2>${character.name}</h2>`;
     main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchHouses();
+  fetchCharacters();
 });
