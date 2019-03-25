8904d768179f54298c8514af659a60465f301636
Jm <jonathan.p.middleton@gmail.com>
2019-03-25 18:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function fetchBooks() {
 
 function renderBooks(json) {
   const main = document.querySelector('main');
-  json.forEach(books => {
+  json.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
